c4bd3bba309228c3f25b3244b516adbf63c3fab8
vikrantpatel-coder <126751705+vikrantpatel-coder@users.noreply.github.com>
2023-03-30 14:56:42 -0400

    string_lab_complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code in this file!
+const currentUser = 'Teothehusky';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!` ;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
+
+
+
+
